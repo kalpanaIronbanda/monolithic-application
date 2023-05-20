5903948bbc3bedbbf0459654d55efaf3cdc078c2
kalpana <kalpanaironbanda@gmail.com>
2023-05-20 10:11:17 +0530

    modified the code to get output side by side

---
+++ frontend/dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yum install nodejs -y
+npm install -g npm@9.6.7
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import './App.css'; // Import your custom CSS file
 
 const App = () => {
   return (
-    <div>
-      <h2>Backend Application 1</h2>
-      <h3>Student List</h3>
-      <iframe src="url of backend api" title="Backend Application 1" />
+    <div className="app">
+      <div className="column column1">
+        <h2 className="header">Backend Application 1</h2>
+        <h3>student list</h3>
+        <iframe src="http://backend-api1-ip/" title="Backend Application 1" className="iframe iframe1" />
+      </div>
 
-      <h2>Backend Application 2</h2>
-      <h3>Employee List</h3>
-      <iframe src="url of frontend api" title="Backend Application 2" />
+      <div className="column column2">
+        <h2 className="header">Backend Application 2</h2>
+        <h3> Employee list </h3>
+        <iframe src="http://backend-api2-ip/" title="Backend Application 2" className="iframe iframe2" />
+      </div>
     </div>
   );
 };
+
 export default App;
